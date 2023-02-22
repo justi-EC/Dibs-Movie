6d3096a8c17c4cef83a9f5d8131e9fcce7a4c962
Yiolrin <yiolrin990930@gmail.com>
2023-02-23 02:14:03 +0900

    검색페이지에 그리드 추가

--- src/pages/Dibs.tsx
+++ src/pages/Dibs.tsx
@@ -2,15 +2,42 @@ import MainHeader from "../components/MainHeader";
 import CommonLayout from "../components/CommonLayout";
 import styled from "styled-components";
 import Empty from "../components/styled/Empty";
+import { appFireStore } from "../firebase/config";
+import { useRecoilValue } from "recoil";
+import { userDataState } from "../utils/atom";
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { UserDataType } from "../utils/Types";
 
 const Dibs = () => {
+  const { uid } = useRecoilValue(userDataState);
+  const userRef = doc(appFireStore, "Users", uid!);
+  const [dibsData, setDibsData] = useState<UserDataType>();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const userDocSnapshot = await getDoc(userRef);
+        const userData = userDocSnapshot.data() as UserDataType;
+        setDibsData(userData);
+      } catch (error: any) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <>
       <CommonLayout>
         <MainHeader>찜 목록</MainHeader>
-        <DefaultSection>
-          <Empty />
-        </DefaultSection>
+        {dibsData?.dibsIdList ? (
+          <DefaultSection>
+            <Empty />
+          </DefaultSection>
+        ) : (
+          <DefaultSection></DefaultSection>
+        )}
       </CommonLayout>
     </>
   );
@@ -22,4 +49,5 @@ const DefaultSection = styled.section`
   display: flex;
   align-items: center;
   justify-content: center;
+  height: 70vh;
 `;
